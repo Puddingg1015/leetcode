1ee292baba710a32e91a589ad436323d9fc30f69
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-01-04 22:06:11 -0700

    Time: 6 ms (29.44%) | Memory: 47 MB (14.67%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode head = new ListNode(0);
+        Comparator<ListNode> comp = new NodeComparator();
+        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(comp);
+        for (int i = 0; i < lists.length; i++) {
+            ListNode node = lists[i];
+            if (node == null) continue;
+            while (node != null) {
+                // System.out.println(node.val);
+                pq.add(node);
+                node = node.next;
+            }
+        }
+        ListNode curr = head;
+        while (!pq.isEmpty()) {
+            curr.next = pq.remove();
+            curr = curr.next;
+            // System.out.println(curr.val + " ");
+            // System.out.println(curr.next);
+        }
+        head = head.next;
+        return head;
+    }
+}
+
+class NodeComparator implements Comparator<ListNode> {
+    @Override
+    public int compare(ListNode x, ListNode y) {
+        if (x.val < y.val) {
+            return -1;
+        }
+        if (x.val > y.val) {
+            return 1;
+        }
+        if (x.val == y.val && x.next == null) {
+            return 1;
+        }
+        return -1;
+    }
+}
