82e5a854f2edb5818fd6a7acd819a504bb4586d1
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-01-04 23:35:22 -0700

    Time: 3 ms (46.23%) | Memory: 39.2 MB (25.94%) - LeetSync

---
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -0,0 +1,29 @@
+#define lng long long int
+
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        // theory: given any number of operations you can reduce the number of negatives to 1 or 0
+        lng negatives = 0;
+        lng sum = 0;
+        int min_val = 100001;
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[i].size(); j++) {
+                int ele = matrix[i][j];
+                if (ele < 0) {
+                    negatives++;
+                    if (-ele < min_val) min_val = -ele;
+                    sum += -ele;
+                } else {
+                    if (ele < min_val) min_val = ele;
+                    sum += ele;
+                }
+            }
+        }
+        if (!(negatives%2)) {
+            return sum;
+        } else {
+            return sum - min_val*2;
+        }
+    }
+};
