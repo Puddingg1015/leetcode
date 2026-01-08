90f5640121fada96a3a10925ea0994925f562832
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-01-07 22:00:39 -0700

    Time: 1296 ms (5.20%) | Memory: 14.1 MB (97.60%) - LeetSync

---
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    // the max dotproduct of subsequences of nums1 ending at i and nums2 ending at j (inclusive)
+    int dotproduct[501][501] = {{0}};
+    // The dotproduct of A from i to j and B from k to l is A[i:]*B[k:]-A[j+1:]*B[l+1:]
+    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+        dotproduct[0][0] =nums1[0] * nums2[0];
+        for (int i = 1; i < nums1.size(); i++) dotproduct[i][0] = max(nums1[i] * nums2[0], dotproduct[i-1][0]);
+        for (int j = 1; j < nums2.size(); j++) dotproduct[0][j] = max(nums1[0] * nums2[j], dotproduct[0][j-1]);
+        
+        cout << "\t";
+        for (int j = 1; j < nums2.size(); j++) cout << nums2[j] << "\t";
+        cout << endl;
+        for (int i = 1; i < nums1.size(); i++) {
+            cout << nums1[i] << "\t";
+            for (int j = 1; j < nums2.size(); j++) {
+                // hopefully this works
+                dotproduct[i][j] = max(max(dotproduct[i-1][j], nums1[i] * nums2[j]), max(dotproduct[i-1][j-1] + nums1[i] * nums2[j], dotproduct[i][j-1])); 
+                cout << dotproduct[i][j] << "\t";
+            }
+            cout << endl << endl;
+        }
+        return dotproduct[nums1.size()-1][nums2.size()-1];
+    }
+
+    int dotProduct(vector<int> nums1, vector<int> nums2) {
+        assert(nums1.size() == nums2.size());
+        int out = 0;
+        for (int i = 0; i < nums1.size(); i++) out += nums1[i] * nums2[i];
+        return out;
+    }
+
+};// 72+80+3+45
