a9774d3c25fbbbe73e788c25b91e7548035e7d18
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-01-09 23:56:26 -0700

    Time: 339 ms (63.96%) | Memory: 24 MB (25.52%) - LeetSync

---
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        min_val = [[0 for col in range(len(s2)+1)] for row in range(len(s1)+1)]; # [i][j] is min COMMON sum ending at s1[:i+1] and s2[:j+1]
+        # if (s1[0] == s2[0]): min_val[0][0] = 0;
+        # else: min_val[0][0] = ord(s1[0]) + ord(s2[0]);
+        for i in range(1, len(s1)+1):
+            min_val[i][0] = min_val[i-1][0] + ord(s1[i-1]);
+        for j in range(1, len(s2)+1):
+            min_val[0][j] = min_val[0][j-1] + ord(s2[j-1]);
+        for i in range(1, len(s1)+1):
+            for j in range(1, len(s2)+1):
+                if (s1[i-1] == s2[j-1]):
+                    min_val[i][j] = min_val[i-1][j-1];
+                else:
+                    min_val[i][j] = min(min_val[i-1][j] + ord(s1[i-1]), min_val[i][j-1] + ord(s2[j-1]));
+        print(" ", end = "\t");
+        [print(s2[coli], end = "\t") for coli in range(len(s2))];
+        print();
+        [print(min_val[rowi]) for rowi in range(len(min_val))];
+        return min_val[len(s1)][len(s2)];
+            
