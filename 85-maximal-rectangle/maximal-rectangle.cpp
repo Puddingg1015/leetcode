4e141b07aff5035c19621a72870567420fb1232c
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-01-11 11:35:14 -0700

    Time: 0 ms (100.00%) | Memory: 17.2 MB (92.98%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -23,7 +23,7 @@ public:
             stack<int> prev_j;
             // sort(cum_matrix[i], cum_matrix[i]+(int)matrix[i].size(), greater<int>());
             forj(matrix[i].size()+1) {
-                cout << "here 1" << endl;
+                // cout << "here 1" << endl;
                 if (!j) {
                     prev_j.push(j);
                     continue;
@@ -36,23 +36,23 @@ public:
                     else cur_area = cum_matrix[i][peak_j]*(min(j, (int)matrix[i].size())-prev_j.top()-1);
                     max_rec[i][j] = max(cur_area, max_rec[i][j]);
                     // else max_rec[i][j] = max(cur_area, max(max_rec[i][j-1], max_rec[i-1][j]));
-                    cout << "here 2" << " " << i << ' ' << j << ' ' << cur_area << ' ' << max_rec[i][j] << endl;
+                    // cout << "here 2" << " " << i << ' ' << j << ' ' << cur_area << ' ' << max_rec[i][j] << endl;
                 }
                 max_rec[i][j] = max(max_rec[i][j], max_rec[i][j-1]);
                 prev_j.push(j);
             }
         }
-        fori(matrix.size()) {
-            forj(matrix[i].size()+1) {
-                cout << cum_matrix[i][j] << " ";
-            } cout << endl;
-        }
-        cout << endl;
-        fori(matrix.size()) {
-            forj(matrix[i].size()+1) {
-                cout << max_rec[i][j] << " ";
-            } cout << endl;
-        }
+        // fori(matrix.size()) {
+        //     forj(matrix[i].size()+1) {
+        //         cout << cum_matrix[i][j] << " ";
+        //     } cout << endl;
+        // }
+        // cout << endl;
+        // fori(matrix.size()) {
+        //     forj(matrix[i].size()+1) {
+        //         cout << max_rec[i][j] << " ";
+        //     } cout << endl;
+        // }
         fori(matrix.size()) {
             out = max(out, max_rec[i][matrix[0].size()]);
         }
