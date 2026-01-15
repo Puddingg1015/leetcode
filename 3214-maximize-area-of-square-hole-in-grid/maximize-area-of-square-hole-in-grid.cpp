653ea29f53a524b0af41043338212c1fa1cb4f2a
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-01-15 00:52:11 -0500

    Time: 0 ms (100.00%) | Memory: 31.8 MB (67.83%) - LeetSync

---
+++ 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
+        int maxh = maxConsecutiveSequenceLen(hBars);
+        int maxv = maxConsecutiveSequenceLen(vBars);
+        int side = min(maxh + 1, maxv + 1);
+        return side * side;
+    }
+
+    int maxConsecutiveSequenceLen(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int out = 1;
+        int starti = 0;
+        for (int i = 1; i <= arr.size(); i++) {
+            if (i == arr.size()) out = max(out, i - starti);
+            else if (arr[i] == arr[i-1] + 1) out = max(out, i - starti + 1);
+            else starti = i;
+        }
+        return out;
+    }
+};
