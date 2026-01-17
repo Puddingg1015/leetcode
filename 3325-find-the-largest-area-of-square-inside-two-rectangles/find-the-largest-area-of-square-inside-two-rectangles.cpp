391c2a7c10a0559bc2c67a0a13d91bd4a3c0e21e
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-01-17 15:40:20 -0500

    Time: 109 ms (45.71%) | Memory: 59.4 MB (56.33%) - LeetSync

--- 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.cpp
+++ 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.cpp
@@ -6,8 +6,9 @@ class Solution {
 public:
     long long largestSquareArea(vector<vector<int>>& bottomLeft, vector<vector<int>>& topRight) {
         int n = bottomLeft.size();
-        vector<vector<int>> inter_bl;
-        vector<vector<int>> inter_tr;
+        // vector<vector<int>> inter_bl;
+        // vector<vector<int>> inter_tr;
+        lng max_side = 0;
         fori(n-1) {
             for (int j = i+1; j < n; j++) {
                 // cout << i << " " << j << endl;
@@ -17,15 +18,19 @@ public:
                 // if (topRight[i][0] >= bottomLeft[j][0] && topRight[i][1] >= bottomLeft[j][1]) {
                 //     inter_bl.push_back(bottomLeft[j]);
                 //     inter_tr.push_back(topRight[i]);
-                inter_bl.push_back(vector<int>{max(bottomLeft[i][0], bottomLeft[j][0]), max(bottomLeft[i][1], bottomLeft[j][1])});
-                inter_tr.push_back(vector<int>{min(topRight[i][0], topRight[j][0]), min(topRight[i][1], topRight[j][1])});
+                int bl_x = max(bottomLeft[i][0], bottomLeft[j][0]);
+                int bl_y = max(bottomLeft[i][1], bottomLeft[j][1]);
+                int tr_x = min(topRight[i][0], topRight[j][0]);
+                int tr_y = min(topRight[i][1], topRight[j][1]);
+                // inter_bl.push_back(vector<int>{max(bottomLeft[i][0], bottomLeft[j][0]), max(bottomLeft[i][1], bottomLeft[j][1])});
+                // inter_tr.push_back(vector<int>{min(topRight[i][0], topRight[j][0]), min(topRight[i][1], topRight[j][1])});
+                max_side = max(max_side, (lng)min(tr_x - bl_x, tr_y - bl_y));
             }
         }
-        lng max_side = 0;
-        fori(inter_bl.size()) {
-            // cout << inter_bl[i][0] << "," << inter_bl[i][1] << "  " << inter_tr[i][0] << "," << inter_tr[i][1] << endl;
-            max_side = max(max_side, (lng) min(inter_tr[i][0] - inter_bl[i][0], inter_tr[i][1] - inter_bl[i][1]));
-        }
+        // fori(inter_bl.size()) {
+        //     // cout << inter_bl[i][0] << "," << inter_bl[i][1] << "  " << inter_tr[i][0] << "," << inter_tr[i][1] << endl;
+        //     max_side = max(max_side, (lng) min(inter_tr[i][0] - inter_bl[i][0], inter_tr[i][1] - inter_bl[i][1]));
+        // }
         return max_side * max_side;
     }
 };
