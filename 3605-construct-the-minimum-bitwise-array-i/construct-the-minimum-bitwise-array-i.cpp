28a45528307f880356021745954218e44dfadfb7
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-01-20 01:18:23 -0500

    Time: 4 ms (23.67%) | Memory: 25.1 MB (78.70%) - LeetSync

---
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
+            ans.push_back(-1);
+            for (int potnext = 0; potnext <= num; potnext++) {
+                if ((potnext|(potnext+1)) == num) {
+                    ans[i] = potnext;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
