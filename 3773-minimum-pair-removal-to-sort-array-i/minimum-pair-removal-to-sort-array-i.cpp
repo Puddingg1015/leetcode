bf35363b6bfea064d8d02a0283de89b9e1601550
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-01-22 01:07:31 -0500

    Time: 0 ms (100.00%) | Memory: 26.6 MB (61.14%) - LeetSync

--- 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
@@ -8,7 +8,7 @@ public:
             non_decreasing = true;
             int min_i = n-1;
             for (int i = n-1; i > 0; i--) {
-                cout << nums[i] << " " << non_decreasing << endl;
+                // cout << nums[i] << " " << non_decreasing << endl;
                 if (nums[i-1] > nums[i]) non_decreasing = false;
                 if (nums[i] + nums[i-1] <= nums[min_i] + nums[min_i - 1]) min_i = i;
             }
