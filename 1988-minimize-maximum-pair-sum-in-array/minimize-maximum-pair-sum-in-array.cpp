efcb60b9408906663b7efaf6af05b0e1ad38406c
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-01-24 16:58:22 -0500

    Time: 181 ms (21.68%) | Memory: 100.1 MB (30.13%) - LeetSync

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
@@ -3,12 +3,8 @@ public:
     int minPairSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        vector<pair<int, int>> pairs;
         int out = 0;
-        for (int i = 0; i < n/2; i++) {
-            pairs.push_back(make_pair(nums[i], nums[n-1-i]));
-            out = max(out, nums[i] + nums[n-1-i]);
-        }
+        for (int i = 0; i < n/2; i++) out = max(out, nums[i] + nums[n-1-i]);
         return out;
     }
 };
