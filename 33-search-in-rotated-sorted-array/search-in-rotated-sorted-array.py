dedff24c7471e2ac28d39317dab0090f0c8ef413
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-05-22 18:55:53 -0600

    Time: 3 ms (4.23%) | Memory: 19.5 MB (10.23%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -0,0 +1,37 @@
+class Solution:
+
+    nums = None;
+    # inclusive
+    def findStart(self, l, r)->int:
+        if (l > r): return -1;
+        m = (l+r)//2;
+        if (m == 0):
+            if (self.nums[-1] > self.nums[0]):
+                return 0;
+        if (self.nums[m - 1] > self.nums[m]):
+            return m;
+        #if (l == r):
+        #    return -1;
+        return max(self.findStart(l, m - 1), self.findStart(m + 1, r))
+
+    def search(self, nums: List[int], target: int) -> int:
+        if (nums[0] == target): return 0;
+        self.nums = nums;
+        s = len(nums);
+        offset = self.findStart(0, s - 1);
+        print(offset)
+        l = 0;
+        r = s - 1;
+        while (l <= r):
+            m = (l + r)//2
+            num = nums[(m + offset) % s];
+            if (num == target):
+                return (m + offset) % s
+            elif (num < target):
+                l = m + 1
+            else:
+                r = m - 1 
+        return -1;
+            
+
+
