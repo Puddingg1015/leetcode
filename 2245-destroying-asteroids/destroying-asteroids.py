aa77e099e3f2798badb0cef031d78350e582ce4c
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-05-30 20:26:01 -0600

    Time: 103 ms (19.00%) | Memory: 34.1 MB (12.48%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.py
+++ 2245-destroying-asteroids/destroying-asteroids.py
@@ -1,6 +1,5 @@
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
         # chat how are people doing this question in O(N) time :wilted_rose:
-        asteroids.sort();
-        for i in range(len(asteroids)): mass += (asteroids[i] if mass >= asteroids[i] else float("-inf"));
-        return (mass > 0);
+        for i in range(max(bool(asteroids.sort()),len(asteroids))): mass += (asteroids[i] if mass >= asteroids[i] else float("-inf"))
+        return (mass > 0)
