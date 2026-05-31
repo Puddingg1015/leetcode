e272eafe67d9849c4bbdee83a94cb63ce04fd671
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-05-30 20:42:11 -0600

    Time: 107 ms (18.04%) | Memory: 33.9 MB (75.43%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.py
+++ 2245-destroying-asteroids/destroying-asteroids.py
@@ -1,4 +1,4 @@
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
-        # one line solution! (amazing i know) Somehow beats 75% mem :P
+        # one line solution! (amazing i know) Somehow beats 85% mem :P
         return (cm:=mass) and (min([(cm := cm + (asteroids[i] if cm >= asteroids[i] else float("-inf"))) for i in range(max(bool(asteroids.sort()),len(asteroids)))]) > 0)
