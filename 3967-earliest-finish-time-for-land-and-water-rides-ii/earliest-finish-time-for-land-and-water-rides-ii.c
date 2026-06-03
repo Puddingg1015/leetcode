7292291bea7e7147215e0817bae82f2c276e4649
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-06-03 15:49:55 -0600

    Time: 7 ms (45.00%) | Memory: 21.6 MB (100.00%) - LeetSync

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.c
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.c
@@ -4,23 +4,12 @@
 
 
 int earliestFinishTime(int* landStartTime, int landStartTimeSize, int* landDuration, int landDurationSize, int* waterStartTime, int waterStartTimeSize, int* waterDuration, int waterDurationSize) {
-    // well that was simple
-    int min_end = 0x7FFFFFFF;
-    int min_land_end = 0x7FFFFFFF;
-    int min_water_end = 0x7FFFFFFF;
-    for (int i = 0; i < landStartTimeSize; i++) {
-        min_land_end = min(min_land_end, landStartTime[i] + landDuration[i]);
-    }
-    for (int j = 0; j < waterStartTimeSize; j++) {
-        int delay = max(0, waterStartTime[j] - min_land_end);
-        min_end = min(min_end, min_land_end + delay + waterDuration[j]);
-    }
-    for (int i = 0; i < waterStartTimeSize; i++) {
-        min_water_end = min(min_water_end, waterStartTime[i] + waterDuration[i]);
-    }
-    for (int j = 0; j < landStartTimeSize; j++) {
-        int delay = max(0, landStartTime[j] - min_water_end);
-        min_end = min(min_end, min_water_end + delay + landDuration[j]);
-    }
+    // well that was simple, time to be goofy
+    // I present: one of the O(MN) solutions everf
+    int min_end = 0x7FFFFFFF, min_land_end = 0x7FFFFFFF, min_water_end = 0x7FFFFFFF;
+    for (int i = 0; i < landStartTimeSize; i++) min_land_end = min(min_land_end, landStartTime[i] + landDuration[i]);
+    for (int j = 0; j < waterStartTimeSize; j++) min_end = min(min_end, min_land_end + max(0, waterStartTime[j] - min_land_end) + waterDuration[j]);
+    for (int i = 0; i < waterStartTimeSize; i++) min_water_end = min(min_water_end, waterStartTime[i] + waterDuration[i]);
+    for (int j = 0; j < landStartTimeSize; j++) min_end = min(min_end, min_water_end + max(0, landStartTime[j] - min_water_end) + landDuration[j]);
     return min_end;
 }
