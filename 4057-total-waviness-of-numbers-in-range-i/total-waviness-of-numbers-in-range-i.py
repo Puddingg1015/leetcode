53818ed510aa0f131853deb2cff070f5638aa77d
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-06-04 00:11:51 -0600

    Time: 267 ms (72.03%) | Memory: 20.2 MB (8.05%) - LeetSync

---
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
@@ -0,0 +1,6 @@
+class Solution:
+    def totalWaviness(self, num1: int, num2: int) -> int:
+        def waveiness(n):
+            if n < 100: return 0
+            return (((a := n%10) + (b := (n//10)%10) + (c := (n//100)%10)) and ((a < b and b > c) or (a > b and b < c))) + waveiness(n//10)
+        return sum(list(map(waveiness, range(num1, num2+1))))
