cd9f68a737fc7d44356d51d835b257f3ed751baa
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-06-04 00:17:09 -0600

    Time: 258 ms (75.10%) | Memory: 20.1 MB (8.05%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
@@ -1,5 +1,4 @@
 class Solution:
     # Goober questions goober solutions
     def totalWaviness(self, num1: int, num2: int) -> int:
-        waveiness = lambda n:0 if n<100 else (((a:=n%10)+(b:=(n//10)%10)+(c:=(n//100)%10))and(a<b and b>c or a>b and b<c))+waveiness(n//10)
-        return sum(list(map(waveiness, range(num1, num2+1))))
+        return sum(list(map(waveiness := lambda n:0 if n<100 else (((a:=n%10)+(b:=(n//10)%10)+(c:=(n//100)%10))and(a<b and b>c or a>b and b<c))+waveiness(n//10), range(num1, num2+1))))
