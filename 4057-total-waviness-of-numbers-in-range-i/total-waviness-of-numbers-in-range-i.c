2d00cbb2153b298373cbb8a6fae97849fe2b122b
Puddingg1015 <113264939+Puddingg1015@users.noreply.github.com>
2026-06-03 23:56:20 -0600

    Time: 2 ms (100.00%) | Memory: 8.9 MB (100.00%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.c
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.c
@@ -1,12 +1,14 @@
+
 int waviness(int num) {
-    int count = 0;
-    if (num < 100) return count;
+    // goober question
+    int iswave = 0;
+    if (num < 100) return 0;
     int a = num % 10;
     num /= 10;
     int b = num % 10;
     int c = (num / 10) % 10;
-    if (a < b && b > c || a > b && b < c) count++;
-    return count + waviness(num);
+    if (a < b && b > c || a > b && b < c) iswave++;
+    return iswave + waviness(num);
 }
 
 int totalWaviness(int num1, int num2) {
